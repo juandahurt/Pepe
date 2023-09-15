7c6610ab954b49030527a718ed18dd9781f28d87
Juan David Hurtado <juandahurtadom@gmail.com>
2023-09-14 19:55:42 -0500

    Add log modifers

--- Sources/Pepe/Pepe.swift
+++ Sources/Pepe/Pepe.swift
@@ -1,6 +1,10 @@
-public struct Pepe {
-    public private(set) var text = "Hello, World!"
+import Foundation
 
-    public init() {
+/// Hi! I'm Pepe, the logger :)
+public struct Pepe {
+    /// A polite function that gives you a logger instance.
+    /// - Returns: A logger instance.
+    public static func loggerPlease() -> PepeLogger {
+        PepeLogger()
     }
 }
