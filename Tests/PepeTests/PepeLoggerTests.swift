865efa709aa1fcf712d77a94a467e6574c77f358
Juan David Hurtado <juandahurtadom@gmail.com>
2023-09-16 16:29:08 -0500

    Add more test

--- Tests/PepeTests/PepeLoggerTests.swift
+++ Tests/PepeTests/PepeLoggerTests.swift
@@ -9,11 +9,22 @@ import XCTest
 @testable import Pepe
 
 final class PepeLoggerTests: XCTestCase {
-    func test_initialState_shouldBeDefaultOne() {
+    func testState_whenInit_shouldBeDefaultOne() {
         let logger = Pepe.loggerPlease()
         XCTAssertEqual(logger.modifiers, [.pepe])
         XCTAssertEqual(logger.writer, .console)
         XCTAssertEqual(logger.executionType, .sync)
     }
+    
+    func testLogging_withHeavyConfig_shouldExecuteWithReasonableTime() {
+        let reasonableTime = 0.5
+        var logger = Pepe.loggerPlease()
+        logger.modifiers = [.pepe, .level, .time]
+        logger.writer = .os(subsystem: "pepe", category: "perf_test")
+        let start = Date()
+        logger.log("message")
+        let end = Date()
+        let interval = end.timeIntervalSinceReferenceDate - start.timeIntervalSinceReferenceDate
+        XCTAssertTrue(interval < reasonableTime)
     }
 }
